f4cbe1c35e0c93fb19319fc2b182188e9c96ed74
Noah <noahbaca2002@icloud.com>
2022-11-16 05:06:48 -0700

    update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html
-      className="h-full scroll-smooth antialiased [font-feature-settings:'ss01']"
-      lang="en"
-    >
+    <Html className="h-full scroll-smooth antialiased" lang="en">
       <Head />
       <body className="flex h-full flex-col bg-[#111] text-white">
         <Main />
