adc2f238ff33d143478e006abefe1fca680a31a1
Noah <noahbaca2002@icloud.com>
2022-11-16 01:56:57 -0700

    remove flowbite, update custom font in tailwind config, remove tailwind forms plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    fontFamily: {
-      sans: ["Rubik", "sans-serif"],
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
     },
-    extend: {},
   },
-  plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
+  plugins: [],
 };
